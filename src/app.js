1d98544ef121a5d0a28d091dd2d0b2b611906c7a
Cristianr <calrod350@gmail.com>
2021-09-21 19:35:38 -0500

    procfile

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const morgan = require('morgan')
 const app = express();
 const indexRoutes = require('./routes/index')
-const port = 3000;
 
 //configuraciones
+app.set('port', process.env.PORT || 3000);
 mongoose.connect('mongodb+srv://root:toor@cluster1.z3h0q.mongodb.net/Cluster1?retryWrites=true&w=majority')
   .then(db => console.log('Connected'))
   .catch(err => console.log(err))
@@ -16,6 +16,6 @@ app.use(express.urlencoded({extended: false}));
 //rutas
 app.use('/items/', indexRoutes)
 
-app.listen(port, ()=>{
+app.listen(app.get('port'), ()=>{
     console.log('Server started');
 });
