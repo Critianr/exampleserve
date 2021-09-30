badf61f6fdee4f4d6db34eef1493e6e3352de869
Cristianr <calrod350@gmail.com>
2021-09-30 00:32:09 -0500

    up

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan')
 const app = express();
 const indexRoutes = require('./routes/index')
-
+const ticketSchema = require('../src/models/tickets');
 //configuraciones
 app.set('port', process.env.PORT || 3000);
 mongoose.connect('mongodb+srv://root:toor@cluster0.q692i.mongodb.net/Cluster0?retryWrites=true&w=majority')
@@ -19,6 +19,11 @@ app.use(express.static(__dirname ));
 //rutas
 // app.use('/items/', indexRoutes)
 app.use('/', indexRoutes);
+router.post('/tickets', async (req, res)=>{
+    const item = await new ticketSchema (req.body);
+    await item.save();
+    res.json({status: 'task saved'});
+});
 
 app.listen(app.get('port'), ()=>{
     console.log('Server started', app.get('port'));
