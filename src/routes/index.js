a5b57b9034af5f20340b7b5fd1e97cf266921758
Cristianr <calrod350@gmail.com>
2021-09-29 20:18:43 -0500

    up

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,43 +6,43 @@ const User = require('../models/user');
 const { token } = require('morgan');
 // peticion get
 // // res se envia
-// router.get('/', async (req, res)=>{
-//     const items = await Item.find();
-//     res.send(items);
-//     console.log('ya llego')
-// });
+router.get('/tickets', async (req, res)=>{
+    const items = await ticketSchema.find();
+    res.send(items);
+    console.log('ya llego')
+});
 // peticion post req/ el que me trae la informacion
-router.post('/tickets', async (req, res)=>{
-    const item = await new ticketSchema (req.body);
-    await item.save();
-    res.json({status: 'task saved'});
-});
-// router.get('/:id', async (req, res)=>{
-//     const item = await Item.findById(req.params.id);
-//     res.send(item);
+// router.post('/tickets', async (req, res)=>{
+//     const item = await new ticketSchema (req.body);
+//     await item.save();
+//     res.json({status: 'task saved'});
 // });
+router.get('/:id', async (req, res)=>{
+    const item = await ticketSchema.findById(req.params.id);
+    res.send(item);
+});
 // ---------------------------------------- auth route
-router.post('/login', async (req, res)=>{
-    try{
-        const {email, password}= req.body;
-        if(!email || !password ) {
-            return res.status(400).json('email adn pass required');
-        }
-        let token = await authservice.login(req.body);
-        if(token){
-           res.status(token.code).json(token);
-        }
-        }catch(error){
-        res.send(error);
-    }
-});
-router.post('/register', async (req, res) => {
-    try{
-        const user = new User(req.body);
-        const userData = await authservice.register(user);
-        res.send(userData);
-    }catch(error){
-        res.send(error)
-    }
-});
+// router.post('/login', async (req, res)=>{
+//     try{
+//         const {email, password}= req.body;
+//         if(!email || !password ) {
+//             return res.status(400).json('email adn pass required');
+//         }
+//         let token = await authservice.login(req.body);
+//         if(token){
+//            res.status(token.code).json(token);
+//         }
+//         }catch(error){
+//         res.send(error);
+//     }
+// });
+// router.post('/register', async (req, res) => {
+//     try{
+//         const user = new User(req.body);
+//         const userData = await authservice.register(user);
+//         res.send(userData);
+//     }catch(error){
+//         res.send(error)
+//     }
+// });
 module.exports = router;
